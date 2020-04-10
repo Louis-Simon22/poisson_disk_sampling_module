7cede02731047238012b744259bc6536bd842600
Louis-Simon22 <22861550+Louis-Simon22@users.noreply.github.com>
2020-04-10 12:25:14 -0400

    formatting

--- poisson_disk_sampling_module.cpp
+++ poisson_disk_sampling_module.cpp
@@ -7,18 +7,19 @@ void PoissonDiskSampling::_bind_methods() {
                        &PoissonDiskSampling::generate);
 }
 
-Vector<Vector2> PoissonDiskSampling::generate(real_t minimum_radius,
-                                              Rect2 extents) {
+Vector<Vector2> PoissonDiskSampling::generate2D(real_t minimum_distance,
+                                                Rect2 extents) {
   const auto pos_min = extents.get_position();
   const auto pos_max = extents.get_position() + extents.get_size();
   const auto min = std::array<real_t, 2>{{pos_min[0], pos_min[1]}};
   const auto max = std::array<real_t, 2>{{pos_max[0], pos_max[1]}};
-  const auto points = thinks::PoissonDiskSampling<real_t, 2, Vector2, Vector2Traits>(
-      minimum_radius, min, max);
+  const auto points =
+      thinks::PoissonDiskSampling<real_t, 2, Vector2, Vector2Traits>(
+          minimum_radius, min, max);
   auto points_array = Vector<Vector2>();
   points_array.resize(points.size());
   for (size_t i = 0; i < points.size(); i++) {
-	points_array.insert(i, points[i]);
+    points_array.insert(i, points[i]);
   }
   return points_array;
 }
